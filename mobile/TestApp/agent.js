389a5604b38217036334963c3944d2984697269b
HoangHoang <hoang.nh@cxagroup.com>
2020-01-06 14:44:24 +0700

    add alternative library  of core Node for RN

--- mobile/TestApp/agent.js
+++ mobile/TestApp/agent.js
@@ -1,16 +1,19 @@
-// import https from 'https';
+import https from 'https-browserify';
 // import fs from 'fs';
 // import path from 'path';
+import RNFS from 'react-native-fs';
+//
+// // // For more `https.Agent` options, see here:
+// // // https://nodejs.org/api/https.html#https_https_request_options_callback
+//
+module.exports = function (name) {
+    const certFile = RNFS.DocumentDirectoryPath+'/alice_cert.pem';
+    const keyFile = RNFS.DocumentDirectoryPath+'/alice_key.pem';
+    // console.log('RNFS.MainBundlePath',RNFS.MainBundlePath+'./');
+    return new https.Agent({
+        cert: RNFS.readFile(certFile),
+        key: RNFS.readFile(keyFile),
+        rejectUnauthorized: false
+    });
+};
 
-// // For more `https.Agent` options, see here:
-// // https://nodejs.org/api/https.html#https_https_request_options_callback
-
-// export default (agent = (name) => {
-// 	const certFile = path.resolve(__dirname, `${name}_cert.pem`);
-// 	const keyFile = path.resolve(__dirname, `${name}_key.pem`);
-// 	return new https.Agent({
-// 		cert: fs.readFileSync(certFile),
-// 		key: fs.readFileSync(keyFile),
-// 		rejectUnauthorized: false
-// 	});
-// });
